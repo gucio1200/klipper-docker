be2b44f722f9f26c565e7c31a79c17f600f67cc6
gucio1200 <kacper@linuxology.pl>
2021-06-09 06:09:51 +0200

    Remove supervisord (#1)

---
+++ moonraker/start-moonraker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+wait_file() {
+  local file="$1"; shift
+  local wait_seconds="${1:-10}"; shift # 10 seconds as default timeout
+
+  until test $((wait_seconds--)) -eq 0 -o -e "$file" ; do sleep 1; done
+
+  ((++wait_seconds))
+}
+
+klippy_uds=/tmp/klippy_uds
+wait_file "$klippy_uds" 10 || {
+  echo "klippy_uds domain socket file missing after waiting for $? seconds: '$klippy_uds' - check if klipper configured correctly and is running"
+  exit 1
+}
+
+chown klippy:klippy /tmp/klippy_uds>>/logs/moonraker-shell.log
+/home/klippy/moonraker-env/bin/python /home/klippy/moonraker/moonraker/moonraker.py -l /logs/moonraker.log -c /home/klippy/.config/moonraker.conf
