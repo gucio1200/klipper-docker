0d1d598147abba36815027d0d84c1c539d21f4b2
Kacper Gogół <kacper@linuxology.pl>
2022-03-27 21:20:35 +0000

    sync

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker-compose pull --no-parallel \
 	&& docker-compose stop \
-	&& docker-compose rm -f \
-	&& docker volume rm $(docker volume ls) \
-	&& docker-compose up -d
+        && docker-compose up -d
+#	&& docker-compose rm -f \
+#	&& docker volume rm $(docker volume ls)
